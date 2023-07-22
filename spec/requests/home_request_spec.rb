fd452b1fbe48ee7208e40cd41c1eec0b60040e02
Melony Franchini <meltravelz@gmail.com>
2023-07-22 15:21:05 -0600

    Add request spec file

---
+++ spec/requests/home_request_spec.rb
@@ -0,0 +1,10 @@
+# NOTE: do we need to test the controllers since no JSON is being rendered but instead delivered to the view?
+
+# require 'rails_helper'
+
+# RSpec.describe "/", type: :request do
+#   describe "#index" do
+#     before(:each) do
+#     end
+#   end
+# end
